a653212b0410e9fe332139582f3d34ef2c262247
coyoterccr <43521587+coyoterccr@users.noreply.github.com>
2018-09-23 14:34:13 -0300

    Add files via upload

---
+++ Python_1.py
@@ -0,0 +1,27 @@
+print ("Aplicación para sumar 03 numeros")
+num1=float(input("Ingrese numero 1 :"))
+num2=float(input("Ingrese numero 2 :"))
+num3=float(input("Ingrese numero 3 :"))
+suma=num1+num2+num3
+print ("la suma es de :", suma)
+input ("Para cerrar la aplicación presione una tecla")
+
+
+
+
+print ("Aplicación para calculo del area de un triangulo")
+base=int(input("Ingrese la base del triangulo :"))
+altura=int(input("Ingrese la altura del triangulo :"))
+area=(base*altura)/2
+print ("El area del triangulo es de :", area)
+input ("Para cerrar la aplicación presione una tecla")
+
+
+
+print ("Aplicación para calculo del promedio de 03 números")
+n1=int(input("Ingrese primer número :"))
+n2=int(input("Ingrese segundo número :"))
+n3=int(input("Ingrese tercer número :"))
+promedio=(n1+n2+n3)/3
+print ("El promedio de los 03 números es de :", promedio)
+input ("Para cerrar la aplicación presione una tecla")
